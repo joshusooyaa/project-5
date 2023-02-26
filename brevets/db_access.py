6f30ed55facf3b81be6a2ce7b9040a3cdeb63c4d
Josh Sawyer <thejoshsawyer@gmail.com>
2023-02-26 00:15:06 -0800

    Added fetch implementation

---
+++ brevets/db_access.py
@@ -0,0 +1,36 @@
+import os 
+from pymongo import MongoClient
+import logging
+
+client = MongoClient(host=f"mongodb://{os.environ['MONGODB_HOSTNAME']}:27017")
+db = client.brevetsdb
+db.races.insert_one({"open_time": "2000-12-20T20:22", "close_time": "2000-12-20T20:22", "cp_dist": "15", "start_time": "2000-12-20T20:22", "brevet_dist": "200"})
+
+
+def brevet_insert(open_time, close_time, cp_dist, start_time, brevet_dist):
+  # Inserts data into the brevetsdb
+  pass
+
+def brevet_find(checkpoint=0):
+  """ Returns data from brevetsdb
+  
+  Data will be in key-value pair
+  {
+    "open_time": "YYYY-MM-DD HH:mm",
+    "close_time": "YYYY-MM-DD HH:mm",   
+    "cp_dist": "#km",
+    
+    "start_time": "YYYY-MM-DD HH:mm", 
+    "brevet_dist": "#km", 
+  }
+  """
+
+  data = list(db.races.find()) # Convert the all the documents to a list of dictionaries
+  
+  # Must pop '_id' from the dictionary since it's not needed, but also because it contains an ObjectID which is
+  # not JSON serializable
+  if data is not None:
+    for document in data:
+      document.pop('_id', None)
+  
+  return list(data)
---
+++ brevets/flask_brevets.py
@@ -0,0 +1,96 @@
+"""
+Replacement for RUSA ACP brevet time calculator
+(see https://rusa.org/octime_acp.html)
+
+"""
+
+import flask
+from flask import request
+import arrow  # Replacement for datetime, based on moment.js
+import acp_times  # Brevet time calculations
+import config
+import logging
+from db_access import brevet_find, brevet_insert
+
+from datetime import timedelta
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+CONFIG = config.configuration()
+
+###
+# Pages
+###
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    app.logger.debug("Main page entry")
+    return flask.render_template('calc.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    return flask.render_template('404.html'), 404
+
+
+###############
+#
+# AJAX request handlers
+#   These return JSON, rather than rendering pages.
+#
+###############
+@app.route("/_calc_times")
+def _calc_times():
+    """
+    Calculates open/close times from miles, using rules
+    described at https://rusa.org/octime_alg.html.
+    Expects one URL-encoded argument, the number of miles.
+    """
+    app.logger.debug("Got a JSON request")
+    km = request.args.get('km', 999, type=float)
+    brev = request.args.get('brev', type=float) # No need for a fallback since there's always a value for brev
+    start_time = request.args.get('bd', type=str)
+    
+    
+    time = timedelta(hours=1.5)
+    time2 = arrow.now()
+    time2 = time + time2
+    
+    app.logger.debug("km={}".format(km))
+    app.logger.debug("request.args: {}".format(request.args))
+
+    open_time = acp_times.open_time(km, brev, arrow.get(start_time)).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, brev, arrow.get(start_time)).format('YYYY-MM-DDTHH:mm')
+    app.logger.debug("open_time={}".format(open_time))
+    result = {"open": open_time, "close": close_time}
+    return flask.jsonify(result=result)
+
+
+@app.route("/_fetch")
+def _fetch_data():
+    data = brevet_find()
+
+    if data is not None:
+        return flask.jsonify(result=data)
+    else:
+        return flask.jsonify(err="No data saved")
+
+
+@app.route("/_insert")
+def _insert_datah():
+    pass
+
+#############
+
+app.debug = CONFIG.DEBUG
+if app.debug:
+    app.logger.setLevel(logging.DEBUG)
+
+if __name__ == "__main__":
+    print("Opening for global access on port {}".format(CONFIG.PORT))
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
